1f2579e64802aacc58322da2bb3579d649e1ccdb
Lee Sumin <117659552+dltnals317@users.noreply.github.com>
2024-06-21 14:49:39 +0900

    [level 0] Title: 두 수의 합, Time: 71.13 ms, Memory: 34.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.js
@@ -0,0 +1,19 @@
+function solution(a, b) {
+    var answer = '';
+    const num1 = BigInt(a)
+    const num2 = BigInt(b)
+    num = num1+num2
+    console.log(num1)
+    console.log(num2)
+    
+    answer = String(num)
+    return answer;
+}
+
+
+/*
+- 형변환 한 후,
+- 더한 후
+-문자열로 바꾸기 
+
+*/
