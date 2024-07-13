d30fc23d1c1622468dbd6b4a7ba3c189e9be34d7
Lee Sumin <117659552+dltnals317@users.noreply.github.com>
2024-07-14 02:40:48 +0900

    [Silver III] Title: 순열 사이클, Time: 344 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.py
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.py
@@ -1,30 +1,42 @@
 from collections import deque
 
-def bfs(dic, start, visited):
-    q = deque([start])
-    while q:
-        node = q.popleft()
-        if not visited[node]:
-            visited[node] = True
-            connected_node = dic[node]
-            if not visited[connected_node]:
-                q.append(connected_node)
-
-def circle_num(lst):
-    graph = {i + 1: lst[i] for i in range(len(lst))}
-    visited = [False] * (len(lst) + 1)
-    cycle_count = 0
+def bfs(dic, start):
+    cnt = 0
+    oneself = []
+    visited = [False] * (len(dic) + 1)
     
-    for i in range(1, len(lst) + 1):
-        if not visited[i]:
-            bfs(graph, i, visited)
-            cycle_count += 1
-            
-    print(cycle_count)
+    # 자기 자신을 가리키는 경우 처리
+    for key, value in dic.items():
+        if key == value:
+            cnt += 1
+            oneself.append(key)
+    
+    for key in oneself:
+        del dic[key]
+        visited[key] = True
+    
+    for key in dic:
+        if not visited[key]:
+            q = deque([key])
+            while q:
+                node = q.popleft()
+                if not visited[node]:
+                    visited[node] = True
+                    connected_node = dic[node]
+                    q.append(connected_node)
+            cnt += 1
+                
+    return cnt
+        
+def circle_num(lst):
+    graph = {idx + 1: num for idx, num in enumerate(lst)}
+    result = bfs(graph, 1)
+    print(result)
 
+# 테스트 케이스 입력 받기
 T = int(input())
 
-for i in range(T):
+for _ in range(T):
     N = int(input())
     lst = list(map(int, input().split()))
     circle_num(lst)
