036fc11f03b478dc6fd852770f76bc8f25b715c3
Lee Sumin <117659552+dltnals317@users.noreply.github.com>
2024-07-23 18:42:11 +0900

    [Silver V] Title: 단어 정렬, Time: 576 ms, Memory: 34196 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,22 +1,28 @@
 n = int(input())
 word_dict = {}
+word_set = set()
 
-for _ in range(n):
-    word = input().strip()
-    length = len(word)
-    if length in word_dict:
+for i in range(n):
+    word = input()
+    length = len(word)    
+    if length in word_dict.keys():
         word_dict[length].add(word)
     else:
         word_dict[length] = set()
         word_dict[length].add(word)
+        
 
-sorted_dict_1 = dict(sorted(word_dict.items()))
 
-for key, value in sorted_dict_1.items():
-    sorted_words = sorted(value)
-    sorted_dict_1[key] = sorted_words
+sorted_dict_1 = dict(sorted(word_dict.items())) # 딕셔러니 정렬방법
 
-for key in sorted(sorted_dict_1.keys()):
+for key,value in sorted_dict_1.items():
+    sorted_value = sorted(value)
+    sorted_dict_1[key] = sorted_value
+    
+
+
+for key in sorted_dict_1.keys():
     for word in sorted_dict_1[key]:
         print(word)
+
 
