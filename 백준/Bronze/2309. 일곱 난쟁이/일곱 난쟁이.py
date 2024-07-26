950a74db80584160d9cc3da194725812b683a791
Lee Sumin <117659552+dltnals317@users.noreply.github.com>
2024-07-26 23:42:56 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,28 +1,27 @@
-sum_of_all = 0
-dwarfs = []
+sum = 0
+lst = []
+for i in range(9):
+    x = int(input())
+    lst.append(x)
+    sum+=x
 
-# 입력 받기
-for _ in range(9):
-    height = int(input())
-    dwarfs.append(height)
-    sum_of_all += height
-
-# 두 난쟁이의 합을 100으로 맞추기 위해 쌍을 찾기
 found = False
 for i in range(8):
-    for j in range(i + 1, 9):
-        if (sum_of_all - (dwarfs[i] + dwarfs[j])) == 100:
-            # 두 난쟁이를 제거
-            dwarfs.pop(j)  # j 번째 요소 제거
-            dwarfs.pop(i)  # i 번째 요소 제거
+    for j in range(i+1,9):
+        value = lst[i] + lst[j]
+        if sum-value == 100:
+            lst.pop(j)  # j 번째 요소 제거
+            lst.pop(i)  # i 번째 요소 제거
             found = True
             break
     if found:
         break
 
-# 결과를 오름차순으로 정렬 후 출력
-dwarfs.sort()
-for height in dwarfs:
-    print(height)
+
+
+lst.sort()
+for x in lst:
+    print(x)
+    
 
     
