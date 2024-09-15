8ddcb3fe7a6b569f4e9d65cdadd3202adb0d6b11
Lee Sumin <117659552+dltnals317@users.noreply.github.com>
2024-09-15 16:21:19 +0900

    [level 2] Title: 할인 행사, Time: 138.71 ms, Memory: 17.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,37 @@
+# 리스트 슬라이싱
+# discount 배열 길이 - 10까지 for문을 돌면서, 리스트 슬라이싱 적용
+
+def solution(want, number, discount):
+    
+    wanted_dict = {}
+    possible_day = []
+    for i in range(len(want)):
+        wanted_dict[want[i]] = number[i]
+    
+    
+    max_day = len(discount)-9
+    
+    for i in range(max_day):
+        item_dict = {}
+        lst = discount[i:i+10]
+        
+        for item in lst :
+            if item in item_dict:
+                item_dict[item]+=1      
+            else:
+                item_dict[item] = 1
+        
+        isPassed = True
+        for item,count in wanted_dict.items():
+            if item in item_dict.keys():
+                if  item_dict[item]< count: 
+                    isPassed = False
+                    break
+            else:
+                isPassed = False
+                break
+            
+        if isPassed:
+            possible_day.append(i+1)
+                
+    return len(possible_day)
