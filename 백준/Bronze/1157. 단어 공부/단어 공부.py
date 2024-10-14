7dfb7a7ec4bbfe49eb368ce604e197e0eb730a8a
Lee Sumin <117659552+dltnals317@users.noreply.github.com>
2024-10-14 23:13:28 +0900

    [Bronze I] Title: 단어 공부, Time: 64 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,22 +1,16 @@
-word = input()
-def find_max_char(word):
-    word= word.upper()
-    word_dict = {}
-    max_cnt = 0
-    for char in word:
-        if char in word_dict.keys():
-            word_dict[char] +=1
-        else:
-            word_dict[char] = 1
-    
-    max_cnt = max(word_dict.values())
-    max_char = [char for char,count in word_dict.items() if count == max_cnt]
+word = input().upper()
+word_lst = list(set(word))
+cnt_lst = []
 
-    if len(max_char) > 1:
-        return "?"
-    else:
-        return max_char[0]     
-    
-rst = find_max_char(word)
-print(rst)
-    
+
+for x in word_lst:
+    cnt = word.count(x)
+    cnt_lst.append(cnt)
+
+
+max_cnt = max(cnt_lst)
+if cnt_lst.count(max_cnt) >=2:
+    print("?")
+else:
+    idx = cnt_lst.index(max_cnt)
+    print(word_lst[idx])
