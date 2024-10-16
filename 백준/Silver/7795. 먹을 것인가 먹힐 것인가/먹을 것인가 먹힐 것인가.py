ddda6ffbd073089a090d0aaafef72167f54df60b
Lee Sumin <117659552+dltnals317@users.noreply.github.com>
2024-10-16 23:25:00 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 272 ms, Memory: 34676 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,29 +1,28 @@
 import sys
 input = sys.stdin.readline
-T = int(input().strip())
 
-
-def binary_search(target,lst):
+def binary_search(target, lst):
     cnt = 0
     left = 0
     right = len(lst) - 1
-    while (left <= right):
+    while left <= right:
         mid = (left + right) // 2
         if lst[mid] < target:
-            cnt = mid+1
+            cnt = mid + 1  # mid를 기준으로 그보다 작은 값의 개수는 mid + 1개
             left = mid + 1
         else:
             right = mid - 1
     return cnt
-       
-    
+
+T = int(input().strip())
+
 for _ in range(T):
-    N,M = map(int,input().split())
+    N, M = map(int, input().split())
     A_lst = [int(x) for x in input().split()]
     B_lst = [int(x) for x in input().split()]
-    count = 0
     B_lst.sort()
+    count = 0
     for x in A_lst:
-        value = binary_search(x,B_lst)
+        value = binary_search(x, B_lst)
         count += value
     print(count)
