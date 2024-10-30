9782ba97770edb3cefc6a017d5d2f01d09178326
Lee Sumin <117659552+dltnals317@users.noreply.github.com>
2024-10-30 17:04:55 +0900

    [Silver I] Title: 지름길, Time: 36 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -1,31 +1,39 @@
-import sys
-input = sys.stdin.readline
 import heapq
+N,D = map(int,input().split())
+
+graph = [[] for _ in range(D+1)]
 INF = int(1e9)
-n,d = map(int,input().split())
-graph = [[] for _ in range (d+1)] 
-distance = [INF] * (d+1)
 
-for i in range(d):
-    graph[i].append((i+1, 1)) #모든 거리
 
-for _ in range(n):
-    s, e, l = map(int, input().split())
-    if e > d :
+#시작점으로부터 거리
+distance = [INF] * (D+1)
+
+for i in range(D):
+    graph[i].append((i+1,1))
+
+
+for _ in range(N):
+    start,end,length = map(int,input().split())
+    if end>D:
         continue
-    graph[s].append((e,l))
-    
+    graph[start].append((end,length))
+
+
 def dijkstra(start):
     q = []
     heapq.heappush(q,(0,start))
     distance[start] = 0
+
     while q:
-        dist,now = heapq.heappop(q)
-        for node,length in graph[now]:
-            cost = dist + length
-            if cost < distance[node]:
-                distance[node] = cost
-                heapq.heappush(q,(cost, node))
-                 
+        now_length,now = heapq.heappop(q)
+        for connected_node_by_now,length_from_now in graph[now]:
+            cost = now_length+length_from_now
+            if distance[connected_node_by_now] > cost:
+                distance[connected_node_by_now] = cost
+                heapq.heappush(q,(cost,connected_node_by_now))
+            
+
+    
+
 dijkstra(0)
-print(distance[d])    
+print(distance[D])
