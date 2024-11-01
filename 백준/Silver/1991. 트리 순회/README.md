83eabdb5221a3cb4d7c52fca856e98dd2b81e8f6
Lee Sumin <117659552+dltnals317@users.noreply.github.com>
2024-11-01 15:56:31 +0900

    [Silver I] Title: 트리 순회, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/README.md
+++ 백준/Silver/1991. 트리 순회/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 31120 KB, 시간: 40 ms
+메모리: 31120 KB, 시간: 32 ms
 
 ### 분류
 
@@ -12,13 +12,13 @@
 
 ### 제출 일자
 
-2024년 4월 4일 15:07:34
+2024년 11월 1일 15:56:20
 
 ### 문제 설명
 
 <p>이진 트리를 입력받아 전위 순회(preorder traversal), 중위 순회(inorder traversal), 후위 순회(postorder traversal)한 결과를 출력하는 프로그램을 작성하시오.</p>
 
-<p style="text-align: center;"><img alt="" src="https://www.acmicpc.net/JudgeOnline/upload/201007/trtr.png" style="height:220px; width:265px"></p>
+<p style="text-align: center;"><img alt="" src="" style="height:220px; width:265px"></p>
 
 <p>예를 들어 위와 같은 이진 트리가 입력되면,</p>
 
--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,42 +1,50 @@
 import sys
 input = sys.stdin.readline
-def preorder(tree,node,lst):
-    if node == ".":
-        return
-    lst.append(node)
-    left_child,right_child = tree[node]
-    preorder(tree,left_child,lst)
-    preorder(tree,right_child,lst)
-
-    return lst
-def inorder(tree,node,lst):
-    if node == "." :
-        return
-    left_child,right_child = tree[node]
-    inorder(tree,left_child,lst)
-    lst.append(node)
-    inorder(tree,right_child,lst)
-    return lst
-def postorder(tree,node,lst):
-    if node == ".":
-        return
-    left_child,right_child = tree[node]
-    postorder(tree,left_child,lst)
-    postorder(tree,right_child,lst)
-    lst.append(node)
-    return lst
-graph = {}
 N = int(input())
+
+
+def preorder(now,order_table):#전위순회
+    if now == ".":
+        return
+    order_table.append(now)
+    left_ch,right_ch = node_graph[now]
+    preorder(left_ch,order_table)
+    preorder(right_ch,order_table)
+   
+    return order_table
+    
+def inorder(now,order_table): #중위순회
+    if now == ".":
+        return
+    left_ch,right_ch = node_graph[now]
+    inorder(left_ch,order_table)
+    order_table.append(now)
+    inorder(right_ch,order_table)
+
+    return order_table
+    
+    
+   
+def postorder(now,order_table): #후위순회
+    if now == ".":
+        return
+    left_ch,right_ch = node_graph[now]
+    postorder(left_ch,order_table)
+    postorder(right_ch,order_table)
+    order_table.append(now)
+
+    return order_table
+
+node_graph = {}
 for _ in range(N):
     parent,left_child,right_child = input().split()
-    graph[parent] = [left_child,right_child]
+    node_graph[parent] = [left_child,right_child]
+    
 
-root_node = list(graph.keys())[0]
-a = preorder(graph,root_node,[])
-b =inorder(graph,root_node,[])
-c = postorder(graph,root_node,[])
+preorder_table = preorder('A',[])
+inorder_table = inorder('A',[])
+postorder_table = postorder('A',[])
 
-
-print("".join(a))
-print("".join(b))
-print("".join(c))
+print("".join(preorder_table))
+print("".join(inorder_table))
+print("".join(postorder_table))
