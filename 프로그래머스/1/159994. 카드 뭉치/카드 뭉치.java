b6208e62e2312524d6cd91bd07032f9376cc7dea
Lee Sumin <117659552+dltnals317@users.noreply.github.com>
2025-02-13 23:52:29 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        
+        String answer = "";
+        
+        
+        int idx1 = 0;
+        int idx2 = 0;
+        
+        for(String word : goal){
+            if (idx1 < cards1.length && word.equals(cards1[idx1])){
+                idx1 ++;
+            }else if (idx2 < cards2.length && word.equals(cards2[idx2])){
+                idx2 ++;
+            } else{
+                return answer = "No";
+            }
+            
+            answer = "Yes";
+        }
+        return answer;
+    }
+}
