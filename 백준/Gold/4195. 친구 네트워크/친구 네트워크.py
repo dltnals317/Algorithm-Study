66c74f96f9ec37ba1e61c15e69871178000db1ef
Lee Sumin <117659552+dltnals317@users.noreply.github.com>
2025-11-21 10:22:28 +0900

    [Gold II] Title: 친구 네트워크, Time: 5600 ms, Memory: 62452 KB -BaekjoonHub

---
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
@@ -0,0 +1,38 @@
+
+# union에서 rank 비교하는 로직도 필요
+
+t = int(input())
+
+    
+def find(x): #루트 찾기
+    if parents[x] !=x :
+        parents[x] = find(parents[x])
+    return parents[x]
+    
+
+
+def union(a, b):
+    ra = find(a)
+    rb = find(b)
+    if ra !=rb:
+        parents[rb] = ra
+        counts[ra] += counts[rb]
+    return counts[ra]
+
+        
+for _ in range(t):
+    parents = {}
+    counts = {}
+    f = int(input())
+    for _ in range(f):
+        a,b = input().split()
+        if a not in parents:
+            parents[a] = a
+            counts[a] = 1
+        if b not in parents:
+            parents[b] = b
+            counts[b] = 1    
+        if find(a) != find(b):
+            union(a, b)
+        root = find(a)
+        print(counts[root])
